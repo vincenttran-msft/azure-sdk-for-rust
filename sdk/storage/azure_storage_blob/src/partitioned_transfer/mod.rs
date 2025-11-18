8e66c5b745ef969fe42f1fbba9b34ee2d16149ef
Vincent Tran <vincenttran@microsoft.com>
2025-11-17 17:11:39 -0800

    run_all_with_concurrency_limit verbose comments

--- sdk/storage/azure_storage_blob/src/partitioned_transfer/mod.rs
+++ sdk/storage/azure_storage_blob/src/partitioned_transfer/mod.rs
@@ -5,71 +5,105 @@ use futures::{
     Stream, TryStreamExt,
 };
 
-type AzureResult<T> = azure_core::Result<T>;
+type AzureResult<T> = azure_core::Result<T>; // Creates a convenient type alias so we don't have to give the full qualified path
 
+// TFut and TError are generic types so that they can work with the different types of futures and errors
 async fn run_all_with_concurrency_limit<TFut, TErr>(
-    mut ops_queue: impl Stream<Item = Result<impl FnOnce() -> TFut, TErr>> + Unpin,
-    parallel: NonZero<usize>,
+    mut ops_queue: impl Stream<Item = Result<impl FnOnce() -> TFut, TErr>> + Unpin, // This closure (impl FnOnce() -> TFut) defines that each operation should only be called exactly once
+    parallel: NonZero<usize>, // NonZero for >=1
 ) -> Result<(), TErr>
 where
-    TFut: Future<Output = Result<(), TErr>>,
+    TFut: Future<Output = Result<(), TErr>>, // This constraint ensures that the future is a contract promised to return a Result or TErr in error case
 {
     let parallel = parallel.get();
 
+    // Get next operation from the operations queue stream
     let first_op = match ops_queue.try_next().await? {
-        Some(item) => item,
-        None => return Ok(()),
+        Some(item) => item,    // Successfully matched an operation
+        None => return Ok(()), // No operations in the queue, return early
     };
 
+    // Calling first_op() actually executes the closure and returns a Future
+    // Box::Pin to keep the future on the heap
+    // future::select_all() creates a future that will complete when any of the futures in the vector are complete
+    // So on first execution, this will just be the first operation we got from the queue
+    // This is the events of "currently running operations"
     let mut get_next_completed_op_future = future::select_all(vec![Box::pin(first_op())]);
+
+    // This is the next closure from the queue to be executed
+    // New events will come from here
     let mut get_next_queue_op_future = ops_queue.try_next();
     loop {
         // while max parallel running ops, focus on just running ops
-        let mut running_ops = get_next_completed_op_future.into_inner();
+        let mut running_ops = get_next_completed_op_future.into_inner(); // Gets the underlying vector of futures (these are the currently running operations)
         while running_ops.len() >= parallel {
-            let result;
-            (result, _, running_ops) = future::select_all(running_ops).await;
-            result?
+            // Loop condition is if we are not at max parallelism
+            let result; // This will hold the Result of the completed operation, if any
+            (result, _, running_ops) = future::select_all(running_ops).await; // This returns (Result of completed Future, idx of Future [ignored], remaining Futures)
+            result? // Propagate any errors
         }
-        get_next_completed_op_future = future::select_all(running_ops);
+        get_next_completed_op_future = future::select_all(running_ops); // Put back the remaining running ops into the select_all future for next iteration
 
+        // Future::select() creates a race condition between get_next_queue_op_future and get_next_completed_op_future
+        // This select() will return whichever Future comes back first
         match future::select(get_next_queue_op_future, get_next_completed_op_future).await {
-            future::Either::Left((Err(e), _)) => return Err(e),
-            future::Either::Right(((Err(e), _, _), _)) => return Err(e),
+            // Below is just error handling for both futures in either case
+            future::Either::Left((Err(e), _)) => return Err(e), // New operation from input queue failed
+            future::Either::Right(((Err(e), _, _), _)) => return Err(e), // An operation from currently running ops failed
+
+            // This is the matching case for new operation from input queue returns first
 
             // next op in the queue arrived first
+            // Ok(next_op_in_queue) is the new operation from the input queue that finished first
+            // running_ops_fut is the future of currently running operations that didn't finish first
             future::Either::Left((Ok(next_op_in_queue), running_ops_fut)) => {
-                get_next_queue_op_future = ops_queue.try_next();
-                get_next_completed_op_future = running_ops_fut;
+                get_next_queue_op_future = ops_queue.try_next(); // Prepare next operation from input queue for next iteration since we just finished the old one
+                get_next_completed_op_future = running_ops_fut; // Put back the running ops future for next iteration
 
+                // Examine what we got from the queue
                 match next_op_in_queue {
                     Some(op) => {
-                        running_ops = get_next_completed_op_future.into_inner();
-                        running_ops.push(Box::pin(op()));
+                        // Operation found (not end of queue)
+                        running_ops = get_next_completed_op_future.into_inner(); // Get the inner Vec of currently running operations
+                        running_ops.push(Box::pin(op())); // Push the completed operation onto the running ops
                         get_next_completed_op_future = future::select_all(running_ops);
                     }
+                    // Queue is empty, no more operations to start
                     // queue was finished, race is over
                     None => break,
                 }
             }
+            // This is the matching case for a running operation returns first
+
             // a running op completed first
+            // Ok(_) ignores the result, _ is the idx, remaining_running_ops is the remaining running ops, next_op_fut is the next op from the queue (didn't win in this case)
             future::Either::Right(((Ok(_), _, remaining_running_ops), next_op_fut)) => {
                 // select panics on empty iter, so we can't race in this case.
                 // forcibly wait for next op in queue and handle it before continuing.
                 if remaining_running_ops.is_empty() {
+                    // As comments state above, this is a special case we need to handle for empty vector
                     let next_op = match next_op_fut.await? {
-                        Some(item) => item,
-                        None => return Ok(()),
+                        // Forcibly wait for next operation to complete
+                        Some(item) => item,    // If there is one match it
+                        None => return Ok(()), // If there is no more operations left
                     };
-                    get_next_queue_op_future = ops_queue.try_next();
+                    get_next_queue_op_future = ops_queue.try_next(); // Prepare for next iteration since we just consumed current next_op_fut
                     get_next_completed_op_future = future::select_all(vec![Box::pin(next_op())]);
+                // Puts the next_op we just completed into the completed ops future
+
+                // This is the normal case where the Vector is non-empty and there are still jobs to run
                 } else {
-                    get_next_queue_op_future = next_op_fut;
+                    get_next_queue_op_future = next_op_fut; // Put back next_op_fut back for next iteration since it didn't win the race in this case
                     get_next_completed_op_future = future::select_all(remaining_running_ops);
+                    // Put back remaining running ops for next iteration
                 }
             }
         }
     }
+
+    // This is to handle after breaking out of the main loop
+    // Gets the underlying Vec of running operations, and if non-empty waits for them to complete
+    // This only runs all currently running operations to completion after the input queue is exhausted, as no new operations need to be started (there are none left to start)
 
     let mut running_ops = get_next_completed_op_future.into_inner();
     while !running_ops.is_empty() {
